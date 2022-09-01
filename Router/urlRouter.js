1fd04199546b6549da1fbf784d4feb911273e84d
Ahmed Nazir <AhmedNazir@users.noreply.github.com>
2022-09-01 16:18:04 +0600

    Add files via upload

--- Router/urlRouter.js
+++ Router/urlRouter.js
@@ -5,7 +5,6 @@ const crypto = require("crypto");
 const router = express.Router();
 
 const randomString = require("../utils/app");
-const { Query } = require("mongoose");
 
 router.get("/random/string/:length", (req, res) => {
     const length = req.params.length ? req.params.length : 5;
@@ -14,7 +13,7 @@ router.get("/random/string/:length", (req, res) => {
 
 router.get("/random/number/:length", (req, res) => {
     const length = req.params.length ? req.params.length : 5;
-    res.status(200).json(Math.floor(Math.random()*1000));
+    res.status(200).json(Math.floor(Math.random() * 1000));
 });
 
 router.get("/:id", (req, res) => {
@@ -25,6 +24,47 @@ router.get("/:id", (req, res) => {
                 status: true,
                 data,
             });
+    });
+});
+
+router.get("/add/:link/:alias", async (req, res) => {
+    const index = crypto
+        .createHash("sha256")
+        .update(req.params.link)
+        .digest("base64");
+    const query = {
+        index,
+    };
+
+    if (req.params.alias) query.alias = req.params.alias;
+
+    UrlModel.find(query, (err, data) => {
+        if (err) res.status(500).json({ error: { message: err.message } });
+        else {
+            if (data.length > 0)
+                res.status(200).json({ status: true, data: data[0] });
+            else {
+                const alias = req.params.alias
+                    ? req.params.alias
+                    : randomString(process.env.ALIAS_LENGTH);
+
+                const newUrl = new UrlModel({
+                    index,
+                    link: req.params.link,
+                    alias,
+                });
+
+                console.log(newUrl);
+                newUrl.save((err) => {
+                    if (err)
+                        res.status(500).json({
+                            status: false,
+                            error: { message: "alias is not available" },
+                        });
+                    else res.status(200).json({ status: true, data: newUrl });
+                });
+            }
+        }
     });
 });
 
@@ -69,14 +109,25 @@ router.post("/", async (req, res) => {
     });
 });
 
-router.put("/:alias", (req, res) => {
-    //   UrlModel.findOne({ alias: req.params.alias }, (err, data) => {
-    //     if (err) res.status(500).json({ error: err.message });
-    //     res.status(200).json({
-    //       status: "success",
-    //       data,
-    //     });
-    //   });
+router.put("/", (req, res) => {
+    UrlModel.updateOne(
+        { _id: req.body.id },
+        {
+            $set: {
+                link: req.body.link,
+                time: Date.now(),
+            },
+        }
+    );
+
+    UrlModel.findOne({ _id: req.body.id }, (err, data) => {
+        if (err) res.json({ status: false });
+        else
+            res.status(200).json({
+                status: true,
+                data,
+            });
+    });
 });
 
 router.delete("/", (req, res) => {});
